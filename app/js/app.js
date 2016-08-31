2f0ff9e6312b406e6ff9fb8682407c764d83d8cc
shawn <shawn@naturalprofessional.com>
2016-08-31 10:30:11 -0700

    Create setSummaryList function, clearCache function, create shell for setdetailPage

---
+++ app/js/app.js
@@ -0,0 +1,58 @@
+$(document).ready(function(){
+
+  clearCache();
+  // setView();
+  setSummaryList()
+
+}) // close doc ready
+
+var baseUrl = "http://localhost:3000/"
+function clearCache() {
+  $.ajax({
+    // Clears cache
+    url: baseUrl,
+    cache: false
+  })
+}
+
+$(window).bind('hashchange', function(e) {
+  var hash = '';
+  console.log(e)
+  // var hashlength = 0;
+  // hashwith = '';
+  if (location.hash == '') {
+    setSummaryList();
+    document.title = 'Boot Badge'
+  }
+  hash = location.hash;
+  // hashlength = hash.length;
+  setDetailPage();
+
+})
+
+function setView(){
+  window.location.hash
+
+}
+
+function setSummaryList() {
+  $.getJSON( baseUrl, function( data ) {
+    var items = [];
+    $.each( data, function( key, val ) {
+      items.push( "<li id='" + key + "'><a href='#" + val.id + "'>" + val.name + "</a></li>" );
+      });
+      $( "#boots-list").append(items.join( "" ) )
+  }); // close get json
+}
+
+function setDetailPage(){
+  console.log("In the setDetailPage function")
+}
+
+$("a").on("click", function(e){
+  e.preventDefault();
+  // show the boot/:id page
+  // pull through object ????
+  $("h2").append(val.name)
+  // ajax hit server  boot.badges []  .forEach(badge) badge.description badge.total_votes
+})
